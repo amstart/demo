6d138a19fe049408034324203afeab1b7a02ad5b
amstart <jochen.krattenmacher@web.de>
2016-09-28 12:40:43 +0200

    usertest

--- functional_tests.py
+++ functional_tests.py
@@ -1,9 +1,24 @@
 from selenium import webdriver
 from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
+import unittest
 
-browser = webdriver.Firefox(firefox_binary=FirefoxBinary(
-    firefox_path='C:\\Program Files\\Mozilla FirefoxESR\\firefox.exe'
-))
-browser.get('http://localhost:8000')
 
-assert 'demoslogic' in browser.title
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Firefox(firefox_binary=FirefoxBinary(
+            firefox_path='C:\\Program Files\\Mozilla FirefoxESR\\firefox.exe'))
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        # Edith has heard about a cool new online to-do app. She goes
+        # to check out its homepage
+        self.browser.get('http://localhost:8000')
+
+        # She notices the page title and header mention to-do lists
+        self.assertIn('demoslogic', self.browser.title)
+        self.fail('Finish the test!')
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
