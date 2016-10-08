a51559bfe96e97edd2281a66118c77f19f780a28
amstart <jochen.krattenmacher@web.de>
2016-10-08 23:14:25 +0200

    functional test whether can vote start

---
+++ functional_tests/test_can_vote.py
@@ -0,0 +1,16 @@
+from selenium import webdriver
+from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
+from selenium.webdriver.common.keys import Keys
+
+from .base import FunctionalTest
+from demoslogic.premises.models import Premise
+
+class UserCanVoteTest(FunctionalTest):
+
+    def test_can_vote(self):
+        self.create_pre_authenticated_session("Alfons")
+        new_premise = Premise(user=self.user, subject="Things", predicate="exist")
+        new_premise.save()
+        self.browser.find_element_by_link_text('Premises').click()
+        self.browser.find_element_by_link_text('Things exist').click()
+        
