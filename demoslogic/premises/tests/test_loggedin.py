9770ecdbae0c8c0c775bb3a7757edd511b1ef764
amstart <jochen.krattenmacher@web.de>
2016-10-20 22:49:36 +0200

    fixtures setup

--- demoslogic/premises/tests/test_loggedin.py
+++ demoslogic/premises/tests/test_loggedin.py
@@ -10,22 +10,24 @@ from ..models import Premise
 premise_core = {'subject':'peas', 'predicate':'make', 'object':'peacocks', 'complement':'cry'}
 
 
-class TemplateTest(TestCase):
-
+class ViewPremiseTest(TestCase):
     def setUp(self):
         # self.factory = RequestFactory()
         self.user = User.objects.create_user(username = 'Alfons')
         self.logged_in = self.client.force_login(user = self.user)
-        # Create the different premises by POST and one by hand
+        self.new_premise = Premise.objects.create(user = self.user)
+        self.detail_url = reverse('premises:detail', args = [str(self.new_premise.pk)])
+
+    def test_vote_form_shows_up(self):
+        response = self.client.get(self.detail_url)
+        self.assertContains(response_user, "id='id_delete'")
 
     def test_delete_button_only_there_for_correct_user(self):
-        new_premise = Premise.objects.create(user = self.user)
         time = timezone.now() - datetime.timedelta(days = 30)
         staged_premise  =  Premise.objects.create(user = self.user)
         setattr(staged_premise, 'pub_date', time)
         staged_premise.save()
-        detail_url = reverse('premises:detail', args = [str(new_premise.pk)])
-        response_user = self.client.get(detail_url)
+        response_user = self.client.get(self.detail_url)
         response_user_staged_premise = self.client.get(reverse('premises:detail', args = [str(staged_premise.pk)]))
         self.assertNotContains(response_user_staged_premise, "id='id_delete'")
         self.assertContains(response_user, "id='id_delete'")
@@ -36,6 +38,12 @@ class TemplateTest(TestCase):
         self.logged_in = self.client.force_login(user = self.otheruser)
         response_otheruser = self.client.get(detail_url)
         self.assertNotContains(response_otheruser, "id='id_delete'")
+
+
+class CreatePremiseTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username = 'Alfons')
+        self.logged_in = self.client.force_login(user = self.user)
 
     def test_premises_show_up_for_everyone_in_relevant_pages(self):
         self.responseFull = self.client.post(reverse('premises:create', args = ['WithComplementedObject']), premise_core)
@@ -56,5 +64,3 @@ class TemplateTest(TestCase):
             self.assertContains(response_index, "class=\"" + key + "\"")
             self.assertContains(response_unstaged, "class=\"" + key + "\"")
             self.assertContains(response_detail, "class=\"" + key + "\"")
-
-    # def test_premises_belong_to_creating_user(self):
