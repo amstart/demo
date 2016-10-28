4319c452a3932edec9ca0b73be17e4b8843fbb24
amstart <jochen.krattenmacher@web.de>
2016-10-28 13:03:56 +0200

    put javascript at the end of the file

--- demoslogic/static/js/project.js
+++ demoslogic/static/js/project.js
@@ -1,4 +1,16 @@
 /* Project specific Javascript goes here. */
+function barplot(data)
+{
+var x = d3.scaleLinear()
+    .domain([0, d3.max(data)])
+    .range([10, 300]);
+d3.select(".barchart")
+    .selectAll("div")
+        .data(data)
+    .enter().append("div")
+        .style("width", function(d) { return x(d) + "px"; })
+        .text(function(d) { return d; });
+}
 
 /*
 Formatting hack to get around crispy-forms unfortunate hardcoding
